cd5b77bc6140f04404e4d240867ccb09f3a5bc59
khalidumar29 <dreamiwoarld.online@gmail.com>
2024-02-15 20:51:32 +0600

    error has been fix

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,7 @@ export default authMiddleware({
     "/events/clerk",
     "/api/webhook/stripe",
     "/api/uploadthing",
+    "/api/webhook/clerk",
   ],
   ignoredRoutes: ["/events/clerk", "/api/webhook/stripe", "/api/uploadthing"],
 });
