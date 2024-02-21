249e4d7c432b0de1a4aaf816d9861bf745036474
Minal Ahmed <mimalahmed6219@gmail.com>
2024-02-21 23:10:06 +0600

    adding delete functionality

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,6 +1,10 @@
 "use server";
 
-import { CreateEventParams } from "@/types";
+import {
+  CreateEventParams,
+  DeleteEventParams,
+  GetAllEventsParams,
+} from "@/types";
 import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../database";
 import Category from "../database/models/category.model";
@@ -46,6 +50,57 @@ export async function getEventById(eventId: string) {
     if (!event) throw new Error("Event not found");
 
     return JSON.parse(JSON.stringify(event));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function getAllEvents({
+  query,
+  limit = 6,
+  page,
+  category,
+}: GetAllEventsParams) {
+  try {
+    await connectToDatabase();
+
+    // const titleCondition = query
+    //   ? { title: { $regex: query, $options: "i" } }
+    //   : {};
+    // const categoryCondition = category
+    //   ? await getCategoryByName(category)
+    //   : null;
+    const conditions = {
+      // $and: [
+      //   titleCondition,
+      //   categoryCondition ? { category: categoryCondition._id } : {},
+      // ],
+    };
+
+    const skipAmount = (Number(page) - 1) * limit;
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const events = await populateEvent(eventsQuery);
+    const eventsCount = await Event.countDocuments(conditions);
+
+    return {
+      data: JSON.parse(JSON.stringify(events)),
+      totalPages: Math.ceil(eventsCount / limit),
+    };
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function deleteEvent({ eventId, path }: DeleteEventParams) {
+  try {
+    await connectToDatabase();
+
+    const deletedEvent = await Event.findByIdAndDelete(eventId);
+    if (deletedEvent) revalidatePath(path);
   } catch (error) {
     handleError(error);
   }
